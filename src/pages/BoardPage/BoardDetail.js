329e3e06659fb541e0592a1336b40f8ee9e0891c
Jihan Park <74421125+bbbjihan@users.noreply.github.com>
2022-12-03 21:05:08 +0900

    feat: ListAlignButton

---
+++ src/pages/BoardPage/BoardDetail.js
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const ComponentLayOut = css`
+`
+
+function BoardDetail(){
+    return(
+        <div css={ComponentLayOut}>
+            BoardDetail
+        </div>
+    )
+}
+
+export default BoardDetail;
