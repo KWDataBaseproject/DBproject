3f20997da9fdc6dc086dde3021633bf59d1ede23
sangpyeong <88651495+sangpyeong@users.noreply.github.com>
2022-12-03 21:07:20 +0900

    9:07

--- src/pages/NewsPage/NewsBoard.js
+++ src/pages/NewsPage/NewsBoard.js
@@ -4,17 +4,17 @@ import image from './image.png';
 
 const ComponentLayOut = css`
     position: fixed;
-    width: 760px;
+    width: 800px;
     height: 800px;
     display: flex;
     flex-direction: column;
     overflow-Y: auto;
     &::-webkit-scrollbar {
-      width: 5px;
-    }
+        width: 5px;
+      }
     &::-webkit-scrollbar-thumb {
-      border-radius: 2px;
-      background: #ccc;
+    border-radius: 2px;
+    background: #ccc;
     }
 `
 
@@ -22,11 +22,13 @@ const Title = css`
     height: 38px;
     font-size: 24px;
     margin-left: 5px;
+    width: 100%;
     color: #ff8906;
 `
 
 const HLineBold = css`
     border: 1px solid black;
+    width: 100%;
     height: 0px;
 `
 
@@ -36,12 +38,20 @@ const HLine = css`
     width: 100%;
 `
 
-const TextLine = css`
-    margin-top: 5px;
-    margin-bottom: 5px;
-    height: 200px;
+const RowLine = css`
+    height: 25%;
     width: 100%;
     display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    
+`
+
+const NewsBox = css`
+    width: 399px;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
     justify-content: space-between;
     &:hover{
         background: #E0E0E0;
@@ -50,61 +60,66 @@ const TextLine = css`
     }
 `
 
-const PostTitle = css`
-    font-size: 15px;
-    width: 20%;
-    line-height: 26px;
-`
-
-const PostWriter = css`
-    font-size: 13px;
-    width: 50%;
-    line-height: 26px;
-    text-align: center;
-`
-
-const PostTime = css`
-    margin-right: 5px;
-    font-size: 15px;
-    width: 20%;
-    line-height: 26px;
-    text-align: right;
-`
 const ImageBox = css`
-    width: 50%;
+    width: 40%;
     margin: 10px;
-    height: 78%;
+    height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-start;
+`
+
+const TitleBox = css`
+    width: 50%;
+    margin: 10px;
+    height: 100%;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
 `
 
 const Image = css`
-    margin-top: 30px;
-    width: 50%;
-    height: 80%;
-`
-
-const BoxRow = css`
-    display: flex;
-    justify-content: space-between;
     width: 100%;
-    height: 100%;
+    height: 50%;
 `
 
-const VLineBold = css`
+const VLine = css`
     border-right: 1px solid black;
     width: 0px;
     height: 100%;
 `
 
-const newBox = css`
-    width: 47%;
-    height: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    margin-right: 5px;
+
+const PostTitle = css`
+    font-size: 18px;
+    width: 100%;
+    line-height: 26px;
+`
+
+const PostWriter = css`
+    font-size: 13px;
+    width: 100%;
+    line-height: 26px;
+    text-align: center;
+`
+
+const PostContent = css`
+    overflow: hidden;  		// 을 사용해 영역을 감출 것
+    text-overflow: ellipsis;  	// 로 ... 을 만들기 
+    white-space: nowrap;
+    font-size: 13px;
+    margin-top: 10px;
+    width: 100%;
+    height: 50%;
+    line-height: 26px;
+    text-align: center;
+`
+
+const PostTime = css`
+    font-size: 15px;
+    width: 100%;
+    line-height: 26px;
+    text-align: center;
 `
 
 function NewsBoard(){
@@ -112,115 +127,119 @@ function NewsBoard(){
         <div css={ComponentLayOut}>
             <div css={Title}>뉴스</div>
             <div css={HLineBold}></div>
-            <div css={TextLine}>
-                <div css={BoxRow}>
-                    <div css={newBox}>
-                        <div css={ImageBox}>
-                            <img css={Image} src={image}></img>
-                            <div css={PostWriter}>서울경제</div>
-                            <div css={PostTime}>2022.10.20</div>
-                        </div>
-                        <div css={PostWriter}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+            <div css={RowLine}>
+                <div css={NewsBox}>
+                    <div css={ImageBox}>
+                        <img css={Image} src={image}></img>
+                        <div css={PostWriter}>서울경제</div>
+                        <div css={PostTime}>2022.10.20</div>
                     </div>
-                    <div css={VLineBold}></div>
-                    <div css={newBox}>
-                        <div css={ImageBox}>
-                            <img css={Image} src={image}></img>
-                            <div css={PostWriter}>서울경제</div>
-                            <div css={PostTime}>2022.10.20</div>
+                    <div css={TitleBox}>
+                        <div css={PostTitle}>달러 약세 vs 저가 매수…환율, 1300원선 출발 예상[외환브리핑]
                         </div>
-                        <div css={PostWriter}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+                        <div css={PostContent}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
                     </div>
-                </div>                             
+                </div>
+                <div css={VLine}></div>
+                <div css={NewsBox}>
+                    <div css={ImageBox}>
+                        <img css={Image} src={image}></img>
+                        <div css={PostWriter}>서울경제</div>
+                        <div css={PostTime}>2022.10.20</div>
+                    </div>
+                    <div css={TitleBox}>
+                        <div css={PostTitle}>달러 약세 vs 저가 매수…환율, 1300원선 출발 예상[외환브리핑]
+                        </div>
+                        <div css={PostContent}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+                    </div>
+                </div>           
             </div>
             <div css={HLine}></div>
-            <div css={TextLine}>
-                <div css={BoxRow}>
-                    <div css={newBox}>
-                        <div css={ImageBox}>
-                            <img css={Image} src={image}></img>
-                            <div css={PostWriter}>서울경제</div>
-                            <div css={PostTime}>2022.10.20</div>
-                        </div>
-                        <div css={PostWriter}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+            <div css={RowLine}>
+                <div css={NewsBox}>
+                    <div css={ImageBox}>
+                        <img css={Image} src={image}></img>
+                        <div css={PostWriter}>서울경제</div>
+                        <div css={PostTime}>2022.10.20</div>
                     </div>
-                    <div css={VLineBold}></div>
-                    <div css={newBox}>
-                        <div css={ImageBox}>
-                            <img css={Image} src={image}></img>
-                            <div css={PostWriter}>서울경제</div>
-                            <div css={PostTime}>2022.10.20</div>
+                    <div css={TitleBox}>
+                        <div css={PostTitle}>달러 약세 vs 저가 매수…환율, 1300원선 출발 예상[외환브리핑]
                         </div>
-                        <div css={PostWriter}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+                        <div css={PostContent}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
                     </div>
-                </div>                             
+                </div>
+                <div css={VLine}></div>
+                <div css={NewsBox}>
+                    <div css={ImageBox}>
+                        <img css={Image} src={image}></img>
+                        <div css={PostWriter}>서울경제</div>
+                        <div css={PostTime}>2022.10.20</div>
+                    </div>
+                    <div css={TitleBox}>
+                        <div css={PostTitle}>달러 약세 vs 저가 매수…환율, 1300원선 출발 예상[외환브리핑]
+                        </div>
+                        <div css={PostContent}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+                    </div>
+                </div>           
             </div>
             <div css={HLine}></div>
-            <div css={TextLine}>
-                <div css={BoxRow}>
-                    <div css={newBox}>
-                        <div css={ImageBox}>
-                            <img css={Image} src={image}></img>
-                            <div css={PostWriter}>서울경제</div>
-                            <div css={PostTime}>2022.10.20</div>
-                        </div>
-                        <div css={PostWriter}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+            <div css={RowLine}>
+                <div css={NewsBox}>
+                    <div css={ImageBox}>
+                        <img css={Image} src={image}></img>
+                        <div css={PostWriter}>서울경제</div>
+                        <div css={PostTime}>2022.10.20</div>
                     </div>
-                    <div css={VLineBold}></div>
-                    <div css={newBox}>
-                        <div css={ImageBox}>
-                            <img css={Image} src={image}></img>
-                            <div css={PostWriter}>서울경제</div>
-                            <div css={PostTime}>2022.10.20</div>
+                    <div css={TitleBox}>
+                        <div css={PostTitle}>달러 약세 vs 저가 매수…환율, 1300원선 출발 예상[외환브리핑]
                         </div>
-                        <div css={PostWriter}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+                        <div css={PostContent}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
                     </div>
-                </div>                             
+                </div>
+                <div css={VLine}></div>
+                <div css={NewsBox}>
+                    <div css={ImageBox}>
+                        <img css={Image} src={image}></img>
+                        <div css={PostWriter}>서울경제</div>
+                        <div css={PostTime}>2022.10.20</div>
+                    </div>
+                    <div css={TitleBox}>
+                        <div css={PostTitle}>달러 약세 vs 저가 매수…환율, 1300원선 출발 예상[외환브리핑]
+                        </div>
+                        <div css={PostContent}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+                    </div>
+                </div>           
             </div>
             <div css={HLine}></div>
-            <div css={TextLine}>
-                <div css={BoxRow}>
-                    <div css={newBox}>
-                        <div css={ImageBox}>
-                            <img css={Image} src={image}></img>
-                            <div css={PostWriter}>서울경제</div>
-                            <div css={PostTime}>2022.10.20</div>
-                        </div>
-                        <div css={PostWriter}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+            <div css={RowLine}>
+                <div css={NewsBox}>
+                    <div css={ImageBox}>
+                        <img css={Image} src={image}></img>
+                        <div css={PostWriter}>서울경제</div>
+                        <div css={PostTime}>2022.10.20</div>
                     </div>
-                    <div css={VLineBold}></div>
-                    <div css={newBox}>
-                        <div css={ImageBox}>
-                            <img css={Image} src={image}></img>
-                            <div css={PostWriter}>서울경제</div>
-                            <div css={PostTime}>2022.10.20</div>
+                    <div css={TitleBox}>
+                        <div css={PostTitle}>달러 약세 vs 저가 매수…환율, 1300원선 출발 예상[외환브리핑]
                         </div>
-                        <div css={PostWriter}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+                        <div css={PostContent}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
                     </div>
-                </div>                             
+                </div>
+                <div css={VLine}></div>
+                <div css={NewsBox}>
+                    <div css={ImageBox}>
+                        <img css={Image} src={image}></img>
+                        <div css={PostWriter}>서울경제</div>
+                        <div css={PostTime}>2022.10.20</div>
+                    </div>
+                    <div css={TitleBox}>
+                        <div css={PostTitle}>달러 약세 vs 저가 매수…환율, 1300원선 출발 예상[외환브리핑]
+                        </div>
+                        <div css={PostContent}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
+                    </div>
+                </div>           
             </div>
             <div css={HLine}></div>
-            <div css={TextLine}>
-                <div css={BoxRow}>
-                    <div css={newBox}>
-                        <div css={ImageBox}>
-                            <img css={Image} src={image}></img>
-                            <div css={PostWriter}>서울경제</div>
-                            <div css={PostTime}>2022.10.20</div>
-                        </div>
-                        <div css={PostWriter}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
-                    </div>
-                    <div css={VLineBold}></div>
-                    <div css={newBox}>
-                        <div css={ImageBox}>
-                            <img css={Image} src={image}></img>
-                            <div css={PostWriter}>서울경제</div>
-                            <div css={PostTime}>2022.10.20</div>
-                        </div>
-                        <div css={PostWriter}>미국 달러인덱스 고용 지표 대기, 104선 하락 중국 위안화 역외서 7.03위안대 등락, 보합권 뉴욕증시 혼조세 마감, 국내증시 외국인 주목</div>
-                    </div>
-                </div>                             
-            </div>
+            
             <div css={HLineBold}></div>
         </div>
     )
