40cb6dea47b4a3c7bfed98edd9ef961166c5d8ed
sangpyeong <88651495+sangpyeong@users.noreply.github.com>
2022-12-05 20:26:13 +0900

    news complete

--- src/pages/NewsPage/NewsPage.js
+++ src/pages/NewsPage/NewsPage.js
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
-import { css, keyframes } from '@emotion/react';
-import NewsBoard from './NewsBoard';
-import NewsPagination from './NewsPagination';
+import { css, keyframes } from "@emotion/react";
+import NewsBoard from "./NewsBoard";
+import NewsPagination from "./NewsPagination";
 const PageRender = keyframes`
     from{
         opacity: 0;
@@ -9,43 +9,40 @@ const PageRender = keyframes`
     to{
         opacity: 1;
     }
-`
+`;
 
 const PageLayOut = css`
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    animation: ${PageRender} 1s;
-`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  animation: ${PageRender} 1s;
+`;
 
 const PageRow = css`
     margin-top: 30px;
     width = 100%;
     display: flex;
-    justify-content: space-between;
-    padding-top: 20px;
-    padding-bottom: 40px;
-`
+    justify-content: center;
+`;
 
 const VLine = css`
-    border-right: 1px solid black;
-    width: 0px;
-`
+  border-right: 1px solid black;
+  width: 0px;
+`;
 
-function NewsPage(){
-    return(
-        <div css={PageLayOut}>
-            <div css={PageRow}>
-                <NewsBoard/>
-            </div>
-            <div css={PageRow}>
-                <NewsPagination/>
-            </div>
-        </div>
-    );
+function NewsPage() {
+  return (
+    <div css={PageLayOut}>
+      <div css={PageRow}>
+        <NewsBoard />
+      </div>
+      <div css={PageRow}>
+        <NewsPagination />
+      </div>
+    </div>
+  );
 }
-
 
 export default NewsPage;
--- src/pages/NewsPage/NewsPagination.js
+++ src/pages/NewsPage/NewsPagination.js
@@ -1,29 +1,40 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
+import { css } from "@emotion/react";
 
 const ComponentLayOut = css`
-    position: fixed;
-    left: 45%;
-    top: 90%;
-    width: 10%;
-`
+  width: 20%;
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+`;
 
+const PageNumber = css`
+  display: flex;
+  justify-content: center;
+  font-size: 13px;
+  margin-top: 10px;
+  width: 10%;
+  height: 100%;
+  line-height: 26px;
+  text-align: center;
+  &:hover {
+    background: #e0e0e0;
+    transform: translateY(0px);
+    transition: 0.3s;
+  }
+`;
 
-function NewsPagination(){
-    return(
-        <div css={ComponentLayOut}>
-            <ul>
-                <li><a href="#">1</a></li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">1</a></li>
-
-            </ul>
-        </div>
-
-    )
+function NewsPagination() {
+  return (
+    <div css={ComponentLayOut}>
+      <div css={PageNumber}>1</div>
+      <div css={PageNumber}>2</div>
+      <div css={PageNumber}>3</div>
+      <div css={PageNumber}>4</div>
+      <div css={PageNumber}>5</div>
+    </div>
+  );
 }
-
 
 export default NewsPagination;
