500b7e9596811545a82f18790b81a768051b6fb6
sangpyeong <88651495+sangpyeong@users.noreply.github.com>
2022-12-11 14:05:33 +0900

    차트 버튼 추가

--- src/pages/StockPage/StockDetail.js
+++ src/pages/StockPage/StockDetail.js
@@ -281,13 +281,40 @@ function StockDetail(){
           }
           options={{
             chart: {
-              height: 350,
-              width: 500,
               toolbar: {
-                show:false,
+                show: true,
+                offsetX: 0,
+                offsetY: 0,
+                tools: {
+                  download: true,
+                  selection: true,
+                  zoom: true,
+                  zoomin: true,
+                  zoomout: true,
+                  pan: true,
+                  reset: true | '<img src="/static/icons/reset.png" width="20">',
+                  customIcons: []
+                },
+                export: {
+                  csv: {
+                    filename: undefined,
+                    columnDelimiter: ',',
+                    headerCategory: 'category',
+                    headerValue: 'value',
+                    dateFormatter(timestamp) {
+                      return new Date(timestamp).toDateString()
+                    }
+                  },
+                  svg: {
+                    filename: undefined,
+                  },
+                  png: {
+                    filename: undefined,
+                  }
+                },
+                autoSelected: 'zoom' 
               },
-              background: "transparent",
-            },
+          }
           }
         }
         >
